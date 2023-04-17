8c0f10993f6163f88d3f57a32be65110e3c3d0f6
Marce <Marce@LAPTOP-CR1JCHBA>
2023-04-17 19:17:03 -0300

    proyecto tp2 inicial

---
+++ tp2_Grupo44/src/main/java/ar/edu/unju/fi/TpGrupo44Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo44Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGrupo44Application.class, args);
+	}
+
+}
