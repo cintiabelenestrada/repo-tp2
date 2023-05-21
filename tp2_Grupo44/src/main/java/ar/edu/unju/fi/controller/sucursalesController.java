3ca134f651b7fb27010427ffde4b54b27b7718d7
cintiabelenestrada <cintiabelenestrada@gmail.com>
2023-05-21 15:45:57 -0300

    eliminacion de comentario

--- tp2_Grupo44/src/main/java/ar/edu/unju/fi/controller/sucursalesController.java
+++ tp2_Grupo44/src/main/java/ar/edu/unju/fi/controller/sucursalesController.java
@@ -3,7 +3,6 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-// @Controller
 @Controller
 public class sucursalesController {
     @GetMapping("/sucursales")
