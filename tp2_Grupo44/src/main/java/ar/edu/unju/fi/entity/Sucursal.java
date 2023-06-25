69bebe12f5e778127ff65910882726d969203f64
cintiabelenestrada <cintiabelenestrada@gmail.com>
2023-06-25 14:18:14 -0300

    Estrada-Validaciones_sucursal y se cambia horario por fecha

--- tp2_Grupo44/src/main/java/ar/edu/unju/fi/entity/Sucursal.java
+++ tp2_Grupo44/src/main/java/ar/edu/unju/fi/entity/Sucursal.java
@@ -1,6 +1,6 @@
 package ar.edu.unju.fi.entity;
 
-import java.time.LocalTime;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -16,8 +16,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
@@ -37,37 +39,41 @@ public class Sucursal {
 	private boolean estado;
 
 	@Column(name = "nombre")
-	@NotBlank(message = "Debes introducir un nombre")
-	@Size(min = 5, max = 15, message = "El nombre solo puede contener entre 5 y 15 caracteres")
-	@Pattern(regexp = "[a-zA-Z]+", message = "Debe contener solo letras")
+	@NotBlank(message = "Debes introducir el nombre de la Nueva Sucursal")
+	@Size(min = 3, max = 15, message = "El nombre solo puede contener entre 3 y 15 letras")
+	@Pattern(regexp = "[a-zA-Z]+", message = "El nombre solo debe contener letras")
 	private String nombre;
 
 	@Column(name = "direccion")
-	@NotBlank(message = "Debes introducir una dirección")
-	@Size(min = 5, max = 30, message = "La dirección solo puede contener entre 5 y 30 caracteres")
-	@Pattern(regexp = "[a-zA-Z]+", message = "La dirección solo puede contener letras")
+	@NotBlank(message = "Debes introducir la dirección de la Nueva Sucursal")
+	@Size(min = 5, max = 30, message = "La dirección debe tener entre 5 y 30 letras")
+	@Pattern(regexp = "[a-zA-Z]+", message = "La dirección solo debe tener letras, abajo ingrese la numeracion")
 	private String direccion;
 
-	@NotBlank(message = "Debes ingresar el número")
+	@NotBlank(message = "Debes ingresar el número de la direccion")
 	@PositiveOrZero(message = "Debes ingresar un número positivo")
-	@Pattern(regexp = "[0-9]+", message = "Debe contener solo números")
+	@Pattern(regexp = "[0-9]+", message = "Debe ingresar solo números en esta seccion")
 	@Column(name = "numero_direccion")
 	private String numeroDireccion;
 
-	@NotBlank(message = "Debes introducir un telefono")
-	@Pattern(regexp = "0388-[0-9]{3}-[0-9]{4}", message = "Ingrese un número de telefono válido")
+	@NotBlank(message = "Debes ingresar el numero de telefono de la sucursal")
+	@Pattern(regexp = "0\\d{2}-\\d{7}|\\d{10}", message = "Ingrese un número de teléfono válido, números de teléfono fijo que comienzan con 0 y tienen un código de área de 3 dígitos seguido de un número de 7 dígitos")
 	@Column(name = "telefono")
 	private String telefono;
 
-	@DateTimeFormat(pattern = "HH:mm")
-	@NotNull(message = "Ingrese un horario de apertura")
-	@Column(name = "horario_apertura")
-	private LocalTime horarioApertura;
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	@NotNull(message = "La fecha una fecha de Inicio")
+	@Past(message = "La fecha de inicio debe pasada a la fecha actual")
+	@Column(name = "fecha_inicio")
+	private LocalDate fechaInicio;
 
-	@DateTimeFormat(pattern = "HH:mm")
-	@NotNull(message = "Ingrese un horario de cierre")
-	@Column(name = "horario_cierre")
-	private LocalTime horarioCierre;
+	@DateTimeFormat(pattern = "dd-MM-yyyy")
+	@NotNull(message = "Ingrese una fecha de cierre")
+	@FutureOrPresent(message = "La fecha de cierre debe ser presente o futura")
+	@Column(name = "fecha_cierre")
+	private LocalDate fechaCierre;
+
+
 
 	@Autowired
 	@JoinColumn(name = "provincia_identificador")
@@ -92,8 +98,8 @@ public class Sucursal {
 	 * @param direccion
 	 * @param numeroDireccion
 	 * @param telefono
-	 * @param horarioApertura
-	 * @param horarioCierre
+	 * @param fechaInicio
+	 * @param fechaCierre
 	 * @param provincia representa la ubicación de la sucursal
 	 */
 	public Sucursal(
@@ -103,8 +109,8 @@ public class Sucursal {
 			String direccion,
 			String numeroDireccion,
 			String telefono,
-			LocalTime horarioApertura,
-			LocalTime horarioCierre,
+			@NotNull(message = "La fecha una fecha de Inicio") @Past(message = "La fecha de inicio debe pasada a la fecha actual") LocalDate fechaInicio,
+			LocalDate fechaCierre,
 			Provincia provincia) {
 
 		this.identificador = identificador;
@@ -113,8 +119,8 @@ public class Sucursal {
 		this.direccion = direccion;
 		this.numeroDireccion = numeroDireccion;
 		this.telefono = telefono;
-		this.horarioApertura = horarioApertura;
-		this.horarioCierre = horarioCierre;
+		this.fechaInicio = fechaInicio;
+		this.fechaCierre = fechaCierre;
 		this.provincia = provincia;
 
 	}
@@ -169,20 +175,20 @@ public class Sucursal {
 		this.telefono = telefono;
 	}
 
-	public LocalTime getHorarioApertura() {
-		return horarioApertura;
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
 	}
 
-	public void setHorarioApertura(LocalTime horarioApertura) {
-		this.horarioApertura = horarioApertura;
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
 	}
 
-	public LocalTime getHorarioCierre() {
-		return horarioCierre;
+	public LocalDate getFechaCierre() {
+		return fechaCierre;
 	}
 
-	public void setHorarioCierre(LocalTime horarioCierre) {
-		this.horarioCierre = horarioCierre;
+	public void setFechaCierre(LocalDate fechaCierre) {
+		this.fechaCierre = fechaCierre;
 	}
 
 	public Provincia getProvincia() {
